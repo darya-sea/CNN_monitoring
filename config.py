ebe55d9424710b1074dc42733958d4fc9c074384
darya-sea <darayjavaysh@gmail.com>
2023-02-06 08:54:23 +0100

    Update env var for DATA_FOLDER

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 CURRENT_DIR = os.path.dirname(__file__)
 
 CNN_FOLDER = os.getenv("CNN_FOLDER", f"{CURRENT_DIR}/CNN")
-DATA_FOLDER = os.getenv("CNN_FOLDER", f"{CURRENT_DIR}/DATA")
+DATA_FOLDER = os.getenv("DATA_FOLDER", f"{CURRENT_DIR}/DATA")
 TRAINING_EPOCHS = 50
 
 POOL_SIZE = 6
