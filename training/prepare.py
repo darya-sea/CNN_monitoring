3bdc5612afa2fc50efa030c619639ec81da3575c
darya-sea <darayjavaysh@gmail.com>
2023-02-07 09:07:58 +0100

    Skip errors for rmtree

--- training/prepare.py
+++ training/prepare.py
@@ -28,8 +28,7 @@ class PrepareImages:
       return False
 
     all_classes = os.listdir(self.__input_folder)
-    shutil.rmtree(self.__output_folder)
-    os.makedirs(self.__output_folder, exist_ok=True)
+    shutil.rmtree(self.__output_folder, ignore_errors=True)
 
     for _class in all_classes:
       train_folder = f"{self.__output_folder}/train/{_class}"
