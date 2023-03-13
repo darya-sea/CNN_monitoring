71618200fd154fa0d8d07f3670ebc360c781f307
darya-sea <darayjavaysh@gmail.com>
2023-03-13 08:56:25 +0100

    Update help

--- application/console.py
+++ application/console.py
@@ -51,25 +51,27 @@ def train():
             train_generator, validation_generator, config.TRAINING_EPOCHS)
         # visualization.process_history(history, f"{config.DATA_FOLDER}/output/history")
 
+def help(script_name):
+    print(
+    f"""
+        usage: {script_name} <prepare|train|predict>
+
+        preapre example: 
+          python {script_name} prepare ndvi
+          python {script_name} prepare data
+          python {script_name} prepare 
+        train example: 
+          python {script_name} train
+        predict example: 
+          python {script_name} predict "CNN/heřmánkovec nevonný/2022_09_21 hermankovec/00257C.tif"
+    """
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         match sys.argv[1]:
             case "help":
-                print(
-                    f"""
-        usage: {sys.argv[0]} <prepare|train|predict>
-
-        preapre example: 
-          python main.py prepare ndvi
-          python main.py prepare data
-          python main.py prepare 
-        train example: 
-          python main.py train
-        predict example: 
-          python main.py predict "CNN/heřmánkovec nevonný/2022_09_21 hermankovec/00257C.tif"
-        """
-                )
+                help(sys.argv[0])
             case "prepare":
                 prepare(sys.argv[2] if len(sys.argv) > 2 else None)
             case "train":
@@ -79,3 +81,5 @@ if __name__ == "__main__":
                     predict(sys.argv[2])
                 else:
                     print(f"usage: {sys.argv[0]} predict <image_path>")
+    else:
+        help(sys.argv[0])
