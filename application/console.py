50a4375f2c9c02584c1d5e938ed0ac3fe40c22e8
darya-sea <darayjavaysh@gmail.com>
2023-03-13 09:34:43 +0100

    Check models file

--- application/console.py
+++ application/console.py
@@ -31,13 +31,13 @@ def predict(image_path):
 
     model_file = predict.get_best_model(f"{config.DATA_FOLDER}/output/models")
 
-    classes = predict.load_classes(
-        f"{config.DATA_FOLDER}/validation_classes.json")
+    if model_file:
+        classes = predict.load_classes(
+            f"{config.DATA_FOLDER}/validation_classes.json")
 
-    resutls = predict.predict(image_path, classes, model_file)
-    visualization.show_predicted_images(resutls)
-    pprint(resutls)
-
+        resutls = predict.predict(image_path, classes, model_file)
+        visualization.show_predicted_images(resutls)
+        pprint(resutls)
 
 def train():
     from train.train import Train
