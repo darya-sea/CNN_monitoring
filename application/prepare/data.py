b544a64b6962c635c35f8757af7b7cdc87d96676
darya-sea <darayjavaysh@gmail.com>
2023-03-17 18:00:11 +0100

    Bacground remover, make annotations

--- application/prepare/data.py
+++ application/prepare/data.py
@@ -3,6 +3,7 @@ import os
 import numpy
 import random
 import cv2
+import tempfile
 
 
 class PrepareData:
@@ -10,15 +11,13 @@ class PrepareData:
         self.__input_folder = input_folder
         self.__output_folder = output_folder
 
-    def backround_remove(self, image_path):
-        image = cv2.imread(image_path)
-
+    def remove_background(self, image):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        lower_chlorophyll = numpy.array([40, 50, 50])
-        upper_chlorophyll = numpy.array([80, 255, 255])
+        lower = numpy.array([20, 50, 50])
+        upper = numpy.array([100, 255, 255])
 
-        mask = cv2.inRange(hsv, lower_chlorophyll, upper_chlorophyll)
+        mask = cv2.inRange(hsv, lower, upper)
 
         kernel = numpy.ones((5,5), numpy.uint8)
         mask = cv2.erode(mask, kernel, iterations=1)
@@ -27,7 +26,7 @@ class PrepareData:
         res = cv2.bitwise_and(image, image, mask=mask)
         bg = numpy.zeros_like(image)
         bg[mask != 0] = res[mask != 0]
-        cv2.imwrite(image_path, bg)
+        return bg
 
     def get_all_images(self, folder):
         images = []
@@ -36,28 +35,31 @@ class PrepareData:
             if entry.is_dir():
                 images.extend(self.get_all_images(entry.path))
             else:
-                if entry.path.endswith(("N.tiff", "N.tif")):
+                if entry.path.endswith(("C.tiff", "C.tif")):
                     images.append(entry.path)
 
         return images
 
-    def save_annotations(self, csv_file, image_path, output_image):
+    def make_annotations(self, csv_file, output_image):
         plant_name = os.path.basename(os.path.dirname(output_image))
-        
-        image_name = os.path.basename(output_image).replace("C.", "N.").split("_")[-1]
 
-        ndvi_folder = os.path.join(os.path.dirname(os.path.dirname(image_path)), "NDVI")
-        ndvi_image = os.path.join(ndvi_folder, image_name)
+        if os.path.exists(output_image):
+            image = cv2.imread(output_image)
+            orignal_image = image.copy()
 
-        if os.path.exists(ndvi_image):
-            image = cv2.imread(ndvi_image, cv2.IMREAD_GRAYSCALE)
-            mask = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            image = self.remove_background(image)
+
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            threshold = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
             with open(csv_file, "a") as _file:
-                for contour in cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]:
+                for contour in cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]:
                     (x, y, w, h) = cv2.boundingRect(contour)
-                    if w > 40:
+                    if w > 10 or h > 10:
+                        cv2.rectangle(orignal_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
                         _file.write(f"{output_image},{x},{y},{w},{h},{plant_name}\n")
+            
+            cv2.imwrite(output_image, orignal_image)
 
     def prepare_images(self):
         if self.__input_folder == self.__output_folder:
@@ -108,7 +110,7 @@ class PrepareData:
                     output_image = os.path.join(train_folder, f"{str(random.randrange(1, 100))}_{os.path.basename(image_path)}")
 
                 shutil.copy(image_path, output_image)
-                self.save_annotations(train_annotations, image_path, output_image)
+                self.make_annotations(train_annotations, output_image)
 
             for image_path in validation_images:
                 output_image = os.path.join(validation_folder, os.path.basename(image_path))
@@ -117,4 +119,4 @@ class PrepareData:
                     output_image = os.path.join(validation_folder, f"{str(random.randrange(1, 100))}_{os.path.basename(image_path)}")
 
                 shutil.copy(image_path, output_image)
-                self.save_annotations(validation_annotations, image_path, output_image)
+                self.make_annotations(validation_annotations, output_image)
