73e80e719c8f9a21f413fbe7d7edcd8900c6729b
darya-sea <darayjavaysh@gmail.com>
2023-03-20 15:10:27 +0100

    Removed headers

--- application/prepare/data.py
+++ application/prepare/data.py
@@ -55,8 +55,6 @@ class PrepareData:
             threshold = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
             with open(csv_file, "a") as _file:
-                _file.write(f"image_path,x,y,w,h,plant_name\n")
-
                 for contour in cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]:
                     (x, y, w, h) = cv2.boundingRect(contour)
                     if w > 30 or h > 30:
@@ -67,6 +65,7 @@ class PrepareData:
 
                         #cv2.rectangle(orignal_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
                         _file.write(f"{relative_image_path},{x},{y},{w},{h},{plant_name}\n")
+                        break
             
             #cv2.imwrite(output_image, orignal_image)
 
