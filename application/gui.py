17075ec7928d09b4bfab6b4776d608261eef7e0c
darya-sea <darayjavaysh@gmail.com>
2023-03-24 22:33:15 +0100

    Update gui version

--- application/gui.py
+++ application/gui.py
@@ -2,7 +2,8 @@ import config
 import logging
 import warnings
 import builtins
-import imutils
+import unidecode
+import cv2
 import keras.utils.io_utils
 import matplotlib
 import matplotlib.pyplot
@@ -12,7 +13,6 @@ import PySimpleGUI as gui
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from prepare.data import PrepareData
-from prepare.ndvi import NDVI
 
 from train.train import Train
 from train.prediction import Prediction
@@ -37,7 +37,6 @@ def get_layout():
                    enable_events=True, key="-PREP_DATA_FOLDER-"),
             gui.FolderBrowse(initial_folder=config.DATA_FOLDER, size=(10, 1))
         ],
-        [gui.CBox("Apply NDVI", size=(12, 1), key="-PREP_APPLY_NDVI-")],
         [gui.Button(button_text="Run", size=(10, 1), key="-RUN_PREPARATION-")],
         [gui.Multiline("", disabled=True, background_color="gray", size=(
             75, 20), no_scrollbar=True, key="-PREP_RESULTS-")],
@@ -64,18 +63,24 @@ def get_layout():
     prediction = [
         [
             gui.Text("Model file: ", size=(13, 1)),
-            gui.In(size=(46, 1), enable_events=True, key="-PRED_MODEL_FILE-"),
+            gui.In(
+                size=(46, 1),
+                default_text=Prediction().get_best_model(
+                    os.path.join(config.DATA_FOLDER, "output/models")
+                ),
+                enable_events=True, key="-PRED_MODEL_FILE-"
+            ),
             gui.FileBrowse(initial_folder=config.DATA_FOLDER, size=(10, 1))
         ],
         [
-            gui.Text("Images to detect: ", size=(13, 1)),
+            gui.Text("Image to detect: ", size=(13, 1)),
             gui.In(size=(46, 1), enable_events=True,
                    key="-PRED_IMAGE_FOLDER-"),
-            gui.FolderBrowse(initial_folder=config.DATA_FOLDER, size=(10, 1)),
+            gui.FileBrowse(initial_folder=config.DATA_FOLDER, size=(10, 1)),
         ],
         [
-            gui.Text("Classes file: ", size=(13, 1)),
-            gui.In(default_text=os.path.join(config.DATA_FOLDER, "validation_classes.json"),
+            gui.Text("Plant types file: ", size=(13, 1)),
+            gui.In(default_text=os.path.join(config.DATA_FOLDER, "output/models/train_data_types.json"),
                    size=(46, 1), enable_events=True, key="-PRED_CLASSES_FILE-"),
             gui.FilesBrowse(initial_folder=config.DATA_FOLDER, size=(10, 1))
         ],
@@ -99,13 +104,16 @@ def get_layout():
     return layout
 
 
-def draw_prediction(results):
-    results = results[:12]
+def draw_prediction(results: list, plant_types_file: str):
+    results = results[:4]
 
     images_count = len(results)
 
     if images_count == 0:
         return
+
+    images_in_row = 2
+    images_in_column = 10
 
     count = 1
 
@@ -121,20 +129,47 @@ def draw_prediction(results):
         font=("Arial", 12)
     )
 
-    figure = matplotlib.pyplot.figure(figsize=(15, images_count*3))
-    figure.subplots_adjust(top=0.9, bottom=0.10, left=0.01,
-                           right=0.99, hspace=0.21, wspace=0.5)
+    plant_types = Prediction().load_classes(plant_types_file)
+
+    figure = matplotlib.pyplot.figure(figsize=(images_in_column, images_in_row*images_count))
+    figure.subplots_adjust(top=0.9, bottom=0.01, left=0.01, right=0.99, hspace=0.25, wspace=0.03)
 
     for result in results:
-        for image_path, image_class in result.items():
-            image = matplotlib.image.imread(image_path)
-            image = imutils.opencv2matplotlib(image)
+        image = cv2.imread(result[0])
+        axes = figure.add_subplot(
+            round(images_count/4) + 1 if images_count > 1 else 1, 
+            2 if images_count > 1 else 1, 
+            count
+        )
 
-            axes = figure.add_subplot(round(images_count/4) + 1, 4, count)
-            axes.axis('off')
-            axes.imshow(image, aspect="auto")
-            axes.set_title(image_class, fontsize=9)
-            count += 1
+        plants_on_image = {}
+        plants_on_image_count = 0
+
+        for data in result[1]:
+            x, y, w, h = data["bbox"]
+
+            plant_type = unidecode.unidecode(plant_types[str(data["max_index"])])
+            cv2.putText(image, plant_type, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 1)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+            if plant_type in plants_on_image:
+                plants_on_image[plant_type] += 1
+            else:
+                plants_on_image[plant_type] = 1
+
+            plants_on_image_count += 1
+
+        detected_plants = "\n".join(
+            {
+                f"{plant}: {round(plants_on_image[plant]*100/plants_on_image_count)}%" 
+                for plant in plants_on_image
+            }
+        )
+
+        axes.axis("off")
+        axes.set_title(f"Image: {os.path.basename(result[0])}\n{detected_plants}.", fontsize=9)
+        axes.imshow(image, aspect="auto")
+        count += 1
 
     figure_canvas_agg = FigureCanvasTkAgg(
         figure, sub_window['-CANVAS-'].TKCanvas)
@@ -155,11 +190,11 @@ def perform_long_operation(window, results_key, func, *args):
                 window[results_key].update(message)
         else:
             previous_message = window[results_key].get()
-            window[results_key].update(f'{str(message)}\n{previous_message}')
+            window[results_key].update(f"{previous_message}\n{str(message)}")
 
     def print_wrapper(message, *args, **kwargs):
         previous_message = window[results_key].get()
-        window[results_key].update(f'{str(message)}\n{previous_message}')
+        window[results_key].update(f"{previous_message}\n{str(message)}\n{''.join(args)}")
 
     builtins.print = print_wrapper
 
@@ -171,20 +206,17 @@ def perform_long_operation(window, results_key, func, *args):
     window.write_event_value(f"{results_key[:-1]}_EVENT-", results)
 
 
-def run_preparation(cnn_folder, data_folder, ndvi=False):
-    if ndvi:
-        NDVI().make_ndvi(cnn_folder)
-
+def run_preparation(cnn_folder, data_folder):
     PrepareData(cnn_folder, data_folder).prepare_images()
     window["-PREP_RESULTS-"].update(f'Done!\n{window["-PREP_RESULTS-"].get()}')
 
 
-def run_prediction(image_folder, classes_file, model_file):
+def run_prediction(image_folder, model_file):
+    resutls = []
     window["-PRED_RESULTS-"].update("")
     predict = Prediction()
 
     models_path = os.path.join(config.DATA_FOLDER, "output/models")
-    results_path = os.path.join(os.path.dirname(image_folder), "results.json")
 
     if not values.get("-PRED_MODEL_FILE-"):
         model_file = predict.get_best_model(models_path)
@@ -193,36 +225,27 @@ def run_prediction(image_folder, classes_file, model_file):
         values["-PRED_MODEL_FILE-"] = model_file
         window["-PRED_RESULTS-"].update(f"Loaded best model: {model_file}")
 
-    if classes_file:
-        predict = Prediction()
+    predict = Prediction()
+    resutls = predict.predict(image_folder, model_file)
 
-        classes = predict.load_classes(classes_file)
-        resutls = predict.predict(image_folder, classes, model_file)
-        predict.save_results(results_path, resutls)
+    window["-PRED_RESULTS-"].update(
+        f'Done!\n{window["-PRED_RESULTS-"].get()}')
 
-        window["-PRED_RESULTS-"].update(
-            f'Done!\n{window["-PRED_RESULTS-"].get()}')
-        return resutls
-    else:
-        window["-PRED_RESULTS-"].update(
-            f'Classes file not found!\n{window["-PRED_RESULTS-"].get()}')
-
+    return resutls
 
 def run_training(data_folder, traning_epochs):
     training = Train(data_folder)
     visualization = Visualization()
 
-    classes_path = os.path.join(data_folder, "validation_classes.json")
-    history_path = os.path.join(data_folder, "output")
+    output_folder = os.path.join(data_folder, "output")
 
-    train_generator, validation_generator = training.get_train_generator()
-    
-    training.save_classes(validation_generator, classes_path)
+    train_generator = training.get_data_generator("train")
+    validation_generator = training.get_data_generator("validation")
 
     if train_generator and validation_generator:
         history = training.train(train_generator, validation_generator, traning_epochs)
-        visualization.save_history(history, history_path)
-        visualization.plot_accuracy(history, history_path)
+        visualization.save_history(history, output_folder)
+        visualization.save_traning_plot(history, output_folder)
 
 thread = None
 window = gui.Window("CNN Monitoring", get_layout(), font=("Arial", 12))
@@ -240,8 +263,7 @@ while True:
                     window, "-PREP_RESULTS-",
                     run_preparation,
                     values["-PREP_CNN_FOLDER-"],
-                    values["-PREP_DATA_FOLDER-"],
-                    values["-PREP_APPLY_NDVI-"]
+                    values["-PREP_DATA_FOLDER-"]
                 ),
                 ('-THREAD-', '-THEAD ENDED-')
             )
@@ -270,13 +292,15 @@ while True:
                     "-PRED_RESULTS-",
                     run_prediction,
                     values["-PRED_IMAGE_FOLDER-"],
-                    values["-PRED_CLASSES_FILE-"],
                     values["-PRED_MODEL_FILE-"]
                 ),
                 ('-THREAD-', '-THEAD ENDED-')
             )
         case "-PRED_RESULTS_EVENT-":
-            draw_prediction(values["-PRED_RESULTS_EVENT-"])
+            draw_prediction(
+                values["-PRED_RESULTS_EVENT-"],
+                values["-PRED_CLASSES_FILE-"]
+            )
         case "OK":
             break
         case gui.WIN_CLOSED:
