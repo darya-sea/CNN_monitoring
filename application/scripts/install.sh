64906672a10e2516b2d68241e9713d4b37c228b2
darya-sea <darayjavaysh@gmail.com>
2023-03-27 11:19:24 +0200

    Added remove previous .venv in install.sh

--- application/scripts/install.sh
+++ application/scripts/install.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(dirname $(dirname $0))
 
+rm -rf $SCRIPT_DIR/.venv
 python3 -m virtualenv --python=3.10 $SCRIPT_DIR/.venv
 source $SCRIPT_DIR/.venv/bin/activate
 pip install -r $SCRIPT_DIR/requirements.txt
